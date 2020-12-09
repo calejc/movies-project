7ac1630aac36fd26753fc222513acbc1a05192a6
rkscheer <rkscheer@mail.mccneb.edu>
2020-12-08 20:49:49 -0600

    initial push for updating update.html and success.html

--- src/main/java/cale/spring/Movies/controller/UpdateController.java
+++ src/main/java/cale/spring/Movies/controller/UpdateController.java
@@ -3,6 +3,8 @@ package cale.spring.Movies.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UpdateController {
@@ -12,4 +14,10 @@ public class UpdateController {
         model.addAttribute("pageTitle", "Update database");
         return "update";
     }
+
+    @PostMapping("/success")
+    public String update(@RequestBody Model model) {
+        model.getAttribute("actorName")
+    }
+
 }
