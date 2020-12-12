bdce1e732b113b41cc4b7fe7724c9567200247dc
Cale Cortney <thortney@mailbox.org>
2020-12-12 12:02:08 -0600

    Added dropdown

--- src/main/java/cale/spring/Movies/controller/UpdateController.java
+++ src/main/java/cale/spring/Movies/controller/UpdateController.java
@@ -2,11 +2,10 @@ package cale.spring.Movies.controller;
 
 import cale.spring.Movies.dto.MovieDTO;
 import cale.spring.Movies.model.Movie;
-import cale.spring.Movies.repository.MovieRepository;
 import cale.spring.Movies.service.AuthorizationService;
 import cale.spring.Movies.service.CrudService;
-import cale.spring.Movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.Tuple;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Map;
@@ -30,11 +28,11 @@ public class UpdateController {
     public UpdateController() throws IOException { }
 
     @GetMapping("/update")
-    public String add(Model model, Principal princpal){
+    public String add(Model model, Principal principal){
         return "update";
-//        String userName = getUsername(princpal);
+//        String userName = getUsername(principal);
 //        String authorizations = authorizationMap.get(userName); //create,update
-//        if (authorizations.indexOf("update")>= 0) {
+//        if (authorizations.contains("update")) {
 //            model.addAttribute("pageTitle", "Update database");
 //            return "update";
 //        } else {
@@ -44,20 +42,20 @@ public class UpdateController {
     }
 
     @PostMapping("/addMovie")
-    public String addMovie(@RequestParam("title") String title, @RequestParam("overview") String overview,  Model model, Principal princpal){
-//        String userName = getUsername(princpal);
-//        String authorizations = authorizationMap.get(userName); //create,update
-//        if (authorizations.contains("create")) {
+    public String addMovie(@RequestParam("title") String title, @RequestParam("overview") String overview,  Model model, Principal principal){
+        String userName = getUsername(principal);
+        String authorizations = authorizationMap.get(userName); //create,update
+        if (authorizations.contains("create")) {
             Long id = crudService.generateNewMovieId();
             MovieDTO movie = new MovieDTO(id, title, overview, 5.9);
             Movie savedMovie = crudService.addMovieToDB(movie);
             System.out.println(savedMovie);
             model.addAttribute("successMessage", savedMovie.getTitle());
             return "success";
-//        } else {
-//            model.addAttribute("errorMessage");
-//            return "probably jordan's fault";
-//        }
+        } else {
+            model.addAttribute("errorMessage");
+            return "error";
+        }
     }
 
 
@@ -73,8 +71,20 @@ public class UpdateController {
         }
     }
 
-    private String getUsername(Principal princpal) {
-        return "joe";
+    private String getUsername(Principal principal) {
+        String userName;
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) principal;
+        Map<String, Object> attributes = token.getPrincipal().getAttributes();
+        userName = (String) attributes.get("login");
+        return userName;
+//        try {
+//            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) principal;
+//            Map<String, Object> attributes = token.getPrincipal().getAttributes();
+//            userName = (String) attributes.get("login");
+//            return userName;
+//        } catch (Exception e) {
+//            e.printStackTrace(); //TODO: use a logger!
+//        }
     }
 
     @PostMapping("/success")
