b8e7d429ee64a4af236f08aa6b95aa965c87ca02
Cale Cortney <thortney@mailbox.org>
2020-12-12 09:21:26 -0600

    Added crud forms

--- src/main/java/cale/spring/Movies/model/Genre.java
+++ src/main/java/cale/spring/Movies/model/Genre.java
@@ -1,55 +1,21 @@
-//package cale.spring.Movies.Models;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import java.util.ArrayList;
-//import java.util.List;
-//
+package cale.spring.Movies.model;
+
+import cale.spring.Movies.model.Movie;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
 //@Entity
-//public class Genre {
-//
-//    @Id
-//    int genreId;
-//    String type;
-//    List<Movie> movies;
-//
-//    public List<Movie> getMovies() {
-//        return movies;
-//    }
-//
-//    public void setMovies(List<Movie> movies) {
-//        this.movies = movies;
-//    }
-//
-//    public Genre() {
-//        this(-1, null);
-//        movies = new ArrayList<>();
-//    }
-//
-//    public Genre(int genreId, String type) {
-//        super();
-//        this.genreId = genreId;
-//        this.type = type;
-//    }
-//
-//    public String toString() {
-//        return String.format("Genre(%d,%s)", genreId, type);
-//    }
-//
-//    public int getGenreId() {
-//        return genreId;
-//    }
-//
-//    public void setGenreId(int genreId) {
-//        this.genreId = genreId;
-//    }
-//
-//    public String getType() {
-//        return type;
-//    }
-//
-//    public void setType(String type) {
-//        this.type = type;
-//    }
-//
-//}
+public class Genre {
+
+    @Id
+    int id;
+    String name;
+
+//    @ManyToMany(mappedBy = "genres")
+//    private Set<Movie> movies = new HashSet<>();
+
+}
