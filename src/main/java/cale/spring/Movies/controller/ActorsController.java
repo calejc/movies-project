8a7dc974fecb1ebdf4898c930c32e69696a59dc5
mabrjo <mabrjo64@gmail.com>
2020-12-12 09:42:14 -0600

    adding early beginnings of movies and actors pages

--- src/main/java/cale/spring/Movies/controller/ActorsController.java
+++ src/main/java/cale/spring/Movies/controller/ActorsController.java
@@ -1,15 +1,36 @@
 package cale.spring.Movies.controller;
 
+import cale.spring.Movies.repository.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.transaction.Transactional;
+import java.sql.Date;
+
 @Controller
+@Transactional
 public class ActorsController {
 
+    @Autowired
+    ActorRepository actorRepository;
+
     @GetMapping("/actors")
-    public String actors(Model model){
+    public Model actors(Model model){
         model.addAttribute("pageTitle", "Actors");
-        return "actors";
+
+        return model;
     }
 }
+
+
+
+//    private Long id;
+//    private String name, photoUrl;
+//    private String biography;
+//    private Date birthday, deathday;
+//    private Integer gender;
+//    private Double popularity;
--- src/main/java/cale/spring/Movies/controller/MoviesController.java
+++ src/main/java/cale/spring/Movies/controller/MoviesController.java
@@ -1,15 +1,29 @@
 package cale.spring.Movies.controller;
 
+import cale.spring.Movies.repository.MovieRepository;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.transaction.Transactional;
 
 @Controller
+@Transactional
 public class MoviesController {
 
+    @Autowired
+    MovieRepository movieRepository;
+
+//    @GetMapping("/movie")
+//    public Model movie(@SafeHtml.Attribute())
     @GetMapping("/movies")
     public String movies(Model model){
         model.addAttribute("pageTitle", "Movies");
         return "movies";
     }
 }
+
+//href for actor is /movie/<id> and actor/id/
