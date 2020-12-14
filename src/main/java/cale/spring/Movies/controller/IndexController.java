cc161b948bba183a34b8a8c234bf70d5a356f03f
Cale Cortney <thortney@mailbox.org>
2020-12-14 16:56:05 -0600

    Randomized the photos on index

--- src/main/java/cale/spring/Movies/controller/IndexController.java
+++ src/main/java/cale/spring/Movies/controller/IndexController.java
@@ -1,5 +1,7 @@
 package cale.spring.Movies.controller;
 
+import cale.spring.Movies.model.Actor;
+import cale.spring.Movies.model.Movie;
 import cale.spring.Movies.repository.ActorRepository;
 import cale.spring.Movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class IndexController {
@@ -21,9 +25,23 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         String currDate = (new Date()).toString();
+        List<Actor> actors = actorRepository.findAll();
+        List<Movie> movies = movieRepository.findAll();
+        Actor randomActor = new Actor();
+        do{
+            randomActor = actors.get(returnRand(actors.size()));
+        } while (randomActor.getPhotoUrl().contains("no_img"));
+        Movie randomMovie = movies.get(returnRand(movies.size()));
+        model.addAttribute("randomActor", randomActor);
+        model.addAttribute("randomMovie", randomMovie);
         model.addAttribute("currDate", currDate);
         model.addAttribute("pageTitle", "Movie Home");
         return "index";
+    }
+
+    public Integer returnRand(int listSize){
+        Random rand = new Random();
+        return rand.nextInt(listSize);
     }
 
 }
