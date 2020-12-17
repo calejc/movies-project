21e31186742c8e4367607ddcb583e9c2ac309575
Cale Cortney <thortney@mailbox.org>
2020-12-17 16:03:55 -0600

    Can now add actors to movie

--- src/main/java/cale/spring/Movies/controller/UpdateController.java
+++ src/main/java/cale/spring/Movies/controller/UpdateController.java
@@ -111,23 +111,78 @@ public class UpdateController {
         return mav;
     }
 
+    //TODO
+    // Take in map instead of defined params 'Title' and 'ID'.
+    // Save any updates, continue to actors list edit
     @PostMapping("/edit-movie")
     public ModelAndView editMovie(@RequestParam Long id, @RequestParam String title, ModelAndView mav){
-        System.out.println(movieRepository.findById(id));
-        System.out.printf("ID: %d    ||     Title: %s", id, title);
         movieRepository.updateMovieTitleById(title, id);
         mav.setViewName(String.format("redirect:edit-movie/actors?id=%d", id));
         return mav;
     }
 
     @GetMapping("/edit-movie/actors")
-    public ModelAndView editMoviesActorsForm(@RequestParam("id") Long id, ModelAndView mav, Principal principal){
+    public ModelAndView editMoviesActors(@RequestParam Long id, ModelAndView mav, Principal principal){
+        Authorized authorized = authorized((String) ((OAuth2AuthenticationToken) principal).getPrincipal().getAttributes().get("login"), "update");
+        if (authorized.getAuthorized()){
+            mav.addObject("movieId", id);
+            mav.setViewName("add-actors");
+        } else {
+            mav.addObject("errorMessage", authorized.getReturnMessage());
+        }
+         return mav;
+    }
+
+
+    @RequestMapping(params = "formAction=save", value = "/edit-movie/actors", method=RequestMethod.POST)
+    public ModelAndView editMoviesActorsSave(@RequestParam Map<String, String> allParams, ModelAndView mav){
+        // Create movieDTO
+        Long movieId = Long.parseLong(allParams.get("id"));
+        Movie movie = new Movie();
+        if (movieRepository.findById(movieId).isPresent()){
+            movie = movieRepository.findById(movieId).get();
+        }
+        MovieDTO movieDTO = convertMovieToDTO(movie);
+        System.out.println(movieDTO);
+
+        // Create set of actorDTOs from our form
+        Set<ActorDTO> newActorDTOs = new HashSet<>();
+        List<String> nonActorParams = List.of("id", "q", "formAction");
+        for (Map.Entry<String, String> entry : allParams.entrySet()){
+            if (!nonActorParams.contains(entry.getKey()) && actorRepository.findById(Long.parseLong(entry.getKey())).isPresent()){
+                Actor actor = actorRepository.findById(Long.parseLong(entry.getKey())).get();
+                ActorDTO actorDTO = new ActorDTO(actor.getId(), actor.getName(), actor.getBiography());
+                newActorDTOs.add(actorDTO);
+            }
+        }
+
+        // save
+        for (ActorDTO actorDTO : newActorDTOs){
+            crudService.addMovieToActor(movieDTO, actorDTO);
+        }
+
+        mav.addObject("movie", movieRepository.findById(movieId));
+        mav.setViewName(String.format("redirect:/edit-movie/actors/confirm?id=%d", movieId));
+        return mav;
+    }
+
+    @RequestMapping(params = "formAction=search", value = "/edit-movie/actors", method=RequestMethod.POST)
+    public ModelAndView editMoviesActorsSearch(@RequestParam String q, @RequestParam Long id, ModelAndView mav){
+        List<Actor> actors = actorRepository.findByNameContainingIgnoreCase(q);
+        mav.addObject("movieId", id);
+        mav.addObject("actors", actors);
+        mav.setViewName("add-actors");
+        return mav;
+    }
+
+    @GetMapping("/edit-movie/actors/confirm")
+    public ModelAndView confirmMoviesActors(@RequestParam("id") Long id, ModelAndView mav, Principal principal){
         Authorized authorized = authorized((String) ((OAuth2AuthenticationToken) principal).getPrincipal().getAttributes().get("login"), "update");
         if (authorized.getAuthorized()){
             if (movieRepository.findById(id).isPresent()){
                 Movie movie = movieRepository.findById(id).get();
                 mav.addObject("movie", movie);
-                mav.setViewName("movies-actors");
+                mav.setViewName("edit-movies-actors");
             } else {
                 mav.addObject("errorMessage", String.format("Movie %d not found", id));
                 mav.setViewName("error");
@@ -139,8 +194,8 @@ public class UpdateController {
         return mav;
     }
 
-    @PostMapping("/edit-movie/actors")
-    public ModelAndView editMoviesActors(@RequestParam("id") String id, @RequestParam Map<String, String> allParams, ModelAndView mav, Principal principal){
+    @PostMapping("/edit-movie/actors/confirm")
+    public ModelAndView confirmMoviesActors(@RequestParam("id") String id, @RequestParam Map<String, String> allParams, ModelAndView mav, Principal principal){
 
         Authorized authorized = authorized((String) ((OAuth2AuthenticationToken) principal).getPrincipal().getAttributes().get("login"), "update");
 
@@ -148,6 +203,7 @@ public class UpdateController {
         if (authorized.getAuthorized()){
 
             Long movieId = Long.parseLong(allParams.get("id"));
+//            Long movieId = id;
             Movie movie = new Movie();
             Set<Actor> updatedActors = new HashSet<>();
 
@@ -288,5 +344,21 @@ public class UpdateController {
         return rv;
     }
 
+    public MovieDTO convertMovieToDTO(Movie movie){
+        return new MovieDTO(movie.getId(), movie.getTitle(), movie.getPhotoUrl(), movie.getOverview(), movie.getReleaseDate(), movie.getVoteAverage(), movie.getPopularity(), convertActorSetToDto(movie.getActors()));
+    }
+
+    public Set<ActorDTO> convertActorSetToDto(Set<Actor> actors){
+        Set<ActorDTO> actorDTOs = new HashSet<>();
+        for (Actor actor : actors){
+            actorDTOs.add(convertActorToDto(actor));
+        }
+        return actorDTOs;
+    }
+
+
+    public ActorDTO convertActorToDto(Actor actor){
+        return new ActorDTO(actor.getId(), actor.getName(), actor.getPhotoUrl(), actor.getBiography(), actor.getBirthday(), actor.getDeathday(), actor.getGender(), actor.getPopularity());
+    }
 
 }
