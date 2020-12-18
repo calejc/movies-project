c21d1514e1d7bf51e3e1f65b4697a8380d427b0c
rkscheer <rkscheer@mail.mccneb.edu>
2020-12-17 20:47:19 -0600

    got genre.html pretty well fleshed out.  Jordan didn't help at all.

--- src/main/java/cale/spring/Movies/controller/GenreController.java
+++ src/main/java/cale/spring/Movies/controller/GenreController.java
@@ -1,7 +1,9 @@
 package cale.spring.Movies.controller;
 
 import cale.spring.Movies.model.Genre;
+import cale.spring.Movies.model.Movie;
 import cale.spring.Movies.repository.GenreRepository;
+import cale.spring.Movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Random;
 
 
 @Controller
@@ -18,11 +21,25 @@ public class GenreController {
     @Autowired
     GenreRepository genreRepository;
 
+    @Autowired
+    MovieRepository movieRepository;
+
     @GetMapping("/genres")
     public String genre(Model model) {
         List<Genre> genres = genreRepository.findAll();
+        List<Movie> movies = movieRepository.findAll();
+        Movie randomMovie = new Movie();
+        do{
+            randomMovie = movies.get(returnRand(movies.size()));
+        } while (randomMovie.getPhotoUrl().contains("default"));
+        model.addAttribute("randomMovie", randomMovie);
         model.addAttribute("genres", genres);
         return "genres";
+    }
+
+    public Integer returnRand(int listSize){
+        Random rand = new Random();
+        return rand.nextInt(listSize);
     }
 
 
